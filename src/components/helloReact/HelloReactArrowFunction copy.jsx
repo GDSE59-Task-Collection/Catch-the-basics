118c619091bb8e254acdbff45a207f358eb10248
ThanuraSilva <thanurasilva1989@gmail.com>
2023-04-28 17:20:33 +0530

    Project init with first component

---
+++ src/components/helloReact/HelloReactArrowFunction copy.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//short cut for arrow function based function component is:  alt + ctrl + r ---> rafce
+//short cut for normal function is based component is: alt + ctrl + r ---> rafce
+
+export const HelloReactArrowFunction = () => {
+  return (
+    <>
+    {/* following code encompassed with fragments */}
+    <h1>Hello This is your React lesson series of MERN stack journey</h1>
+    </>
+  )
+}
+
---
+++ src/components/helloReact/HelloReactNormalFunction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//short cut for arrow function based function component is:  alt + ctrl + r ---> rafce
+//short cut for normal function is based component is: alt + ctrl + r ---> rafce
+
+export default function HelloReactNormalFunction() {
+  return (
+    <>
+     <h1>Hello This is your React lesson series of MERN stack journey</h1>
+    </>
+  )
+}
